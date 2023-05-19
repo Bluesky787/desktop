bfb701a1df2c23cf4c88910ddc3cfe3b5dbd28d5
alex-z <blackslayer4@gmail.com>
2023-05-19 20:34:11 +0200

    Bring back ability to encrypt legacy metadata.
    Signed-off-by: alex-z <blackslayer4@gmail.com>

--- src/libsync/owncloudpropagator.h
+++ src/libsync/owncloudpropagator.h
@@ -446,6 +446,10 @@ public:
                               QString &removedDirectory,
                               QString &maybeConflictDirectory);
 
+    void processE2eeMetadataMigration(const SyncFileItemPtr &item,
+                                              QStack<QPair<QString, PropagateDirectory *>> &directories,
+                                              PropagateDirectory *const directoryPropagationJob);
+
     [[nodiscard]] const SyncOptions &syncOptions() const;
     void setSyncOptions(const SyncOptions &syncOptions);
 
@@ -693,7 +697,7 @@ private:
 
     static bool _allowDelayedUpload;
 
-    QSet<QString> _appendedMigrationJobs;
+    QSet<QString> _appendedMigrationJobPaths;
 };
 
 
