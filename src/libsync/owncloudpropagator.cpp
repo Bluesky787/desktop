08f7fc2c157a48debdd56529fc346ffdfabc36d9
Camila <hello@camila.codes>
2023-09-11 19:45:09 +0200

    Add {} for single line if statements.
    Signed-off-by: Camila <hello@camila.codes>

--- src/libsync/owncloudpropagator.cpp
+++ src/libsync/owncloudpropagator.cpp
@@ -1259,8 +1259,9 @@ void PropagatorCompositeJob::finalize()
 {
     // The propagator will do parallel scheduling and this could be posted
     // multiple times on the event loop, ignore the duplicate calls.
-    if (_state == Finished)
+    if (_state == Finished) {
         return;
+    }
 
     _state = Finished;
     emit finished(_hasError == SyncFileItem::NoStatus ? SyncFileItem::Success : _hasError);
